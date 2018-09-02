7b2102b24c87640b487bcc1aa1f9109ba33c7b9a
David Govea <govea.d@gmail.com>
2018-09-01 22:38:02 -0700

    add eslint and tune rules to current style

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    camelcase: 0,
+    'no-var': 0,
+    'keyword-spacing': 0,
+    'vars-on-top': 0,
+    'comma-dangle': 0,
+    'no-param-reassign': 0,
+  },
+};
